24213e2b7d1bd271969ada0b8335933019ceb0e2
Su-daa <subin.kk019@gmail.com>
2024-01-08 12:40:17 +0900

    feat: 결제관련 model, controller, repository 생성

---
+++ src/main/java/site/goldenticket/payment/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package site.goldenticket.payment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import site.goldenticket.payment.model.Payment;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
