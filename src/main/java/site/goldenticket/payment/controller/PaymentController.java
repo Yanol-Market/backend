8725482b4bb64029fc8f106c72c78f18df6436c8
Su-daa <subin.kk019@gmail.com>
2024-01-08 20:33:21 +0900

    feat: 결제 상세 페이지 조회 로직 구현

--- src/main/java/site/goldenticket/payment/controller/PaymentController.java
+++ src/main/java/site/goldenticket/payment/controller/PaymentController.java
@@ -1,7 +1,21 @@
 package site.goldenticket.payment.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import site.goldenticket.common.response.CommonResponse;
+import site.goldenticket.payment.dto.response.PaymentDetailResponse;
+import site.goldenticket.payment.service.PaymentService;
 
 @RestController
+@RequestMapping("/payments")
+@RequiredArgsConstructor
 public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @GetMapping("/{productId}")
+    public CommonResponse<PaymentDetailResponse> getPaymentDetail(@PathVariable("productId") final Long productId) {
+        PaymentDetailResponse paymentDetail = paymentService.getPaymentDetail(productId);
+        return CommonResponse.ok("Payment details retrieved successfully", paymentDetail);
+    }
 }
