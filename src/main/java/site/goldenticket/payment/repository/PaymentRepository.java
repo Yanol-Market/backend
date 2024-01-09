cc472531a36a5353ef402154e586805e1b36411e
Su-daa <subin.kk019@gmail.com>
2024-01-09 10:24:23 +0900

    refactor: jpaRepository 상속에 따른 repository 어노테이션 불필요->삭제

--- src/main/java/site/goldenticket/payment/repository/PaymentRepository.java
+++ src/main/java/site/goldenticket/payment/repository/PaymentRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import site.goldenticket.payment.model.Payment;
 
-@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
