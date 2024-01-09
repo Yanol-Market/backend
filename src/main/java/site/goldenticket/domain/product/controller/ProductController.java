4c40344b1070cbb5952700a1d96f4158c541a3e0
yurim0628 <dbfla0303@naver.com>
2024-01-09 18:49:44 +0900

    feat: 상품 정보 등록 기능 개발

--- src/main/java/site/goldenticket/domain/product/controller/ProductController.java
+++ src/main/java/site/goldenticket/domain/product/controller/ProductController.java
@@ -1,13 +1,24 @@
 package site.goldenticket.domain.product.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import site.goldenticket.common.response.CommonResponse;
+import site.goldenticket.domain.product.dto.ProductRequest;
 import site.goldenticket.domain.product.service.ProductService;
 
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
 public class ProductController {
+
     private final ProductService productService;
+
+    @PostMapping("/{reservationId}")
+    public ResponseEntity<CommonResponse<Long>> createProduct(
+            @PathVariable Long reservationId,
+            @RequestBody ProductRequest productRequest
+    ) {
+        return ResponseEntity.ok(CommonResponse.ok("상품이 성공적으로 등록이 완료되었습니다.", productService.createProduct(productRequest, reservationId)));
+    }
 }
--- src/main/java/site/goldenticket/domain/product/service/ProductService.java
+++ src/main/java/site/goldenticket/domain/product/service/ProductService.java
@@ -3,11 +3,41 @@ package site.goldenticket.domain.product.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.goldenticket.common.constants.ReservationStatus;
+import site.goldenticket.domain.product.dto.ProductRequest;
+import site.goldenticket.domain.product.model.Product;
 import site.goldenticket.domain.product.repository.ProductRepository;
+import site.goldenticket.domain.reservation.model.Reservation;
+import site.goldenticket.domain.reservation.service.ReservationService;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ProductService {
+
     private final ProductRepository productRepository;
+    private final ReservationService reservationService;
+
+    @Transactional
+    public Long createProduct(ProductRequest productRequest, Long reservationId) {
+        Reservation reservation = getReservation(reservationId);
+
+        Product product = productRequest.toEntity(reservation, reservationId);
+        Product savedProduct = saveProduct(product);
+
+        setReservationStatus(reservation, ReservationStatus.REGISTERED);
+        saveReservation(reservation);
+
+        return savedProduct.getId();
+    }
+
+    private Reservation getReservation(Long reservationId) { return reservationService.getReservation(reservationId); }
+
+    private Product saveProduct(Product product) { return productRepository.save(product); }
+
+    private void setReservationStatus(Reservation reservation,ReservationStatus reservationStatus) { reservation.setReservationStatus(reservationStatus); }
+
+    private void saveReservation(Reservation reservation) { reservationService.saveReservation(reservation); }
+
 }
