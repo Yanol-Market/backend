87a004cba9098bf75fc5b07d1dd2525ea0e6082e
yurim0628 <dbfla0303@naver.com>
2024-01-09 22:51:52 +0900

    feat: 예외 추가

--- src/main/java/site/goldenticket/domain/product/service/ProductService.java
+++ src/main/java/site/goldenticket/domain/product/service/ProductService.java
@@ -5,11 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import site.goldenticket.common.constants.ReservationStatus;
+import site.goldenticket.common.exception.CustomException;
 import site.goldenticket.domain.product.dto.ProductRequest;
-import site.goldenticket.domain.product.model.Product;
 import site.goldenticket.domain.product.repository.ProductRepository;
 import site.goldenticket.domain.reservation.model.Reservation;
 import site.goldenticket.domain.reservation.service.ReservationService;
+
+import static site.goldenticket.common.response.ErrorCode.PRODUCT_ALREADY_EXISTS;
 
 @Service
 @RequiredArgsConstructor
@@ -21,23 +23,18 @@ public class ProductService {
 
     @Transactional
     public Long createProduct(ProductRequest productRequest, Long reservationId) {
-        Reservation reservation = getReservation(reservationId);
+        Reservation reservation = reservationService.getReservation(reservationId);
+        checkReservationStatus(reservation.getReservationStatus());
 
-        Product product = productRequest.toEntity(reservation, reservationId);
-        Product savedProduct = saveProduct(product);
+        reservation.setReservationStatus(ReservationStatus.REGISTERED);
+        reservationService.saveReservation(reservation);
 
-        setReservationStatus(reservation, ReservationStatus.REGISTERED);
-        saveReservation(reservation);
-
-        return savedProduct.getId();
+        return productRepository.save(productRequest.toEntity(reservation, reservationId)).getId();
     }
 
-    private Reservation getReservation(Long reservationId) { return reservationService.getReservation(reservationId); }
-
-    private Product saveProduct(Product product) { return productRepository.save(product); }
-
-    private void setReservationStatus(Reservation reservation,ReservationStatus reservationStatus) { reservation.setReservationStatus(reservationStatus); }
-
-    private void saveReservation(Reservation reservation) { reservationService.saveReservation(reservation); }
-
+    private void checkReservationStatus(ReservationStatus reservationStatus) {
+        if (ReservationStatus.REGISTERED.equals(reservationStatus)) {
+            throw new CustomException(PRODUCT_ALREADY_EXISTS);
+        }
+    }
 }
