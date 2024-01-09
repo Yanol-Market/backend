fd7a823cd86964a42b96628496f0f4c12e2b6516
cyPark95 <parkcy1007@gmail.com>
2024-01-09 21:39:43 +0900

    fix: SignatureException 참조 변경

--- src/main/java/site/goldenticket/common/security/authentication/TokenProvider.java
+++ src/main/java/site/goldenticket/common/security/authentication/TokenProvider.java
@@ -3,6 +3,7 @@ package site.goldenticket.common.security.authentication;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,6 @@ import site.goldenticket.common.security.authentication.dto.Token;
 
 import javax.crypto.SecretKey;
 import java.security.Key;
-import java.security.SignatureException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
