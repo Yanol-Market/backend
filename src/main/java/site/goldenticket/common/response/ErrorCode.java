aea4a7f5ef66b659181b4c4a23c99b36a7cafe72
cyPark95 <parkcy1007@gmail.com>
2024-01-09 21:50:34 +0900

    fix: 존재하지 않는 리소스에 대한 HttpStatus 404로 수정

--- src/main/java/site/goldenticket/common/response/ErrorCode.java
+++ src/main/java/site/goldenticket/common/response/ErrorCode.java
@@ -12,7 +12,7 @@ public enum ErrorCode {
 
     COMMON_SYSTEM_ERROR(INTERNAL_SERVER_ERROR, "시스템 오류입니다."),
     COMMON_INVALID_PARAMETER(BAD_REQUEST, "요청한 값이 올바르지 않습니다."),
-    COMMON_RESOURCE_NOT_FOUND(BAD_REQUEST, "존재하지 않는 리소스입니다."),
+    COMMON_RESOURCE_NOT_FOUND(NOT_FOUND, "존재하지 않는 리소스입니다."),
     COMMON_ENTITY_NOT_FOUND(BAD_REQUEST, "존재하지 않는 엔티티입니다."),
 
     // Auth
