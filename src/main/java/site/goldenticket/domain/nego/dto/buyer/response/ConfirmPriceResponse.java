4b2afbab7b103b6d4fbe71460eeb1d1d644f4395
WestSilver99 <seo606101@naver.com>
2024-01-10 15:20:53 +0900

    feat : 가격 승인 Response에 만료시간 추가

--- src/main/java/site/goldenticket/domain/nego/dto/buyer/response/ConfirmPriceResponse.java
+++ src/main/java/site/goldenticket/domain/nego/dto/buyer/response/ConfirmPriceResponse.java
@@ -4,14 +4,18 @@ import lombok.Builder;
 import lombok.Getter;
 import site.goldenticket.domain.nego.entity.Nego;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 public class ConfirmPriceResponse {
     private Boolean consent;
+    private LocalDateTime expirationTime;
 
     public static ConfirmPriceResponse fromEntity(Nego nego){
         return ConfirmPriceResponse.builder()
                 .consent(nego.getConsent())
+                .expirationTime(nego.getExpirationTime())
                 .build();
     }
 
