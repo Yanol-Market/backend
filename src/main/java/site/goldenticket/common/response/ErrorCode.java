0d77a51bf572e1610124836a895a935956c9b62e
Su-daa <subin.kk019@gmail.com>
2024-01-11 20:52:08 +0900

    feat: 아임포트 에러 메세지 작성

--- src/main/java/site/goldenticket/common/response/ErrorCode.java
+++ src/main/java/site/goldenticket/common/response/ErrorCode.java
@@ -13,7 +13,8 @@ public enum ErrorCode {
     COMMON_SYSTEM_ERROR(INTERNAL_SERVER_ERROR, "시스템 오류입니다."),
     COMMON_INVALID_PARAMETER(BAD_REQUEST, "요청한 값이 올바르지 않습니다."),
     COMMON_ENTITY_NOT_FOUND(BAD_REQUEST, "존재하지 않는 엔티티입니다."),
-    PRODUCT_NOT_ON_SALE(FORBIDDEN, "현재 판매중이지 않은 상품입니다"),
+    PRODUCT_NOT_ON_SALE(NOT_FOUND, "현재 판매중이지 않은 상품입니다"),
+    IAMPORT_ERROR(INTERNAL_SERVER_ERROR, "PG사 연동 오류입니다")
     ;
 
     private final HttpStatus httpStatus;
