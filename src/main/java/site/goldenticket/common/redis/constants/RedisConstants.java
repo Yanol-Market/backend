8f6dc733e3388bcdf1c300869a3c8a9ae3f07c3a
yurim0628 <dbfla0303@naver.com>
2024-01-12 04:06:27 +0900

    feat: Redis 관련 상수 추가

---
+++ src/main/java/site/goldenticket/common/redis/constants/RedisConstants.java
@@ -0,0 +1,9 @@
+package site.goldenticket.common.redis.constants;
+
+public class RedisConstants {
+    private RedisConstants() {}
+
+    public static final long MAX_SEARCH_HISTORY_SIZE = 5L;
+    public static final double INCREMENT_AMOUNT = 1.0;
+    public static final int TOP_RANKING_COUNT = 10;
+}
