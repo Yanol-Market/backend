dc07377a0e4c0be9fc7fb1c3f29990e4192c2e81
Su-daa <subin.kk019@gmail.com>
2024-01-12 19:26:35 +0900

    feat: 아임포트(포트원) 결제 단건 조회

--- src/main/java/site/goldenticket/payment/repository/IamportRepository.java
+++ src/main/java/site/goldenticket/payment/repository/IamportRepository.java
@@ -2,10 +2,12 @@ package site.goldenticket.payment.repository;
 
 import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.request.PrepareData;
+import com.siot.IamportRestClient.response.IamportResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import site.goldenticket.common.exception.CustomException;
 import site.goldenticket.common.response.ErrorCode;
+import site.goldenticket.payment.model.Payment;
 
 import java.math.BigDecimal;
 
@@ -13,10 +15,22 @@ import java.math.BigDecimal;
 @RequiredArgsConstructor
 public class IamportRepository {
     private final IamportClient iamportClient;
+    private final PaymentMapper paymentMapper;
 
     public void prepare(Long orderId, BigDecimal price) {
         try {
             iamportClient.postPrepare(new PrepareData(String.valueOf(orderId), price));
+        } catch (Exception e) {
+            throw new CustomException(ErrorCode.IAMPORT_ERROR);
+        }
+    }
+
+    public Payment findPaymentByImpUid(String impUid) {
+        try {
+            IamportResponse<com.siot.IamportRestClient.response.Payment> response =
+                    iamportClient.paymentByImpUid(impUid);
+            //TODO: 결제 취소 결과 올때는 결제 취소 디테일에 저장, 지금은 결제 취소 건은 생각 안할거임~
+            return paymentMapper.mapFrom(response.getResponse());
         } catch (Exception e) {
             throw new CustomException(ErrorCode.IAMPORT_ERROR);
         }
