8b86be4585f7663ad3a9c99fd5f2a5d402616fe0
yurim0628 <dbfla0303@naver.com>
2024-01-13 02:07:34 +0900

    feat: 전체 지역 및 전체 가격과 관련된 상태 값 추가

--- src/main/java/site/goldenticket/common/constants/PriceRange.java
+++ src/main/java/site/goldenticket/common/constants/PriceRange.java
@@ -10,7 +10,8 @@ public enum PriceRange {
     BETWEEN_10_AND_20("10만원대", 100000, 200000),
     BETWEEN_20_AND_30("20만원대", 200000, 300000),
     BETWEEN_30_AND_40("30-40만원대", 300000, 400000),
-    ABOVE_50("50만원 이상", 500000, null);
+    ABOVE_50("50만원 이상", 500000, null),
+    FULL_RANGE("전체 가격", null, null);
 
     private final String label;
     private final Integer minPrice;
