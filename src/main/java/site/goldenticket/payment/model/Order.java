3ef1045a0cf9515337ea695a1b83d7d118bef854
Su-daa <subin.kk019@gmail.com>
2024-01-12 19:22:19 +0900

    feat: 주문 상태 값 설정 메서드 작성

--- src/main/java/site/goldenticket/payment/model/Order.java
+++ src/main/java/site/goldenticket/payment/model/Order.java
@@ -1,10 +1,7 @@
 package site.goldenticket.payment.model;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import site.goldenticket.common.constants.OrderStatus;
@@ -45,5 +42,9 @@ public class Order {
 
     public Integer getTotalPrice() {
         return (int) (this.price*1.05);
+    }
+
+    public void updateStatus(OrderStatus status) {
+        this.status = status;
     }
 }
