46896b755632b64d8e2d224a6dc03312c779def0
cyPark95 <parkcy1007@gmail.com>
2024-01-13 18:01:48 +0900

    feat: 사용자 등록 서비스 로직 구현

--- src/main/java/site/goldenticket/domain/user/service/UserService.java
+++ src/main/java/site/goldenticket/domain/user/service/UserService.java
@@ -2,8 +2,12 @@ package site.goldenticket.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import site.goldenticket.domain.user.dto.JoinRequest;
+import site.goldenticket.domain.user.dto.JoinResponse;
+import site.goldenticket.domain.user.entity.User;
 import site.goldenticket.domain.user.repository.UserRepository;
 
 @Slf4j
@@ -13,6 +17,7 @@ import site.goldenticket.domain.user.repository.UserRepository;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public boolean isExistEmail(String email) {
         log.info("Duplicated Check Email = {}", email);
@@ -22,5 +27,13 @@ public class UserService {
     public boolean isExistNickname(String nickname) {
         log.info("Duplicated Check Nickname = {}", nickname);
         return userRepository.existsByNickname(nickname);
+    }
+
+    @Transactional
+    public JoinResponse join(JoinRequest joinRequest) {
+        String encodePassword = passwordEncoder.encode(joinRequest.password());
+        User user = joinRequest.toEntity(encodePassword);
+        userRepository.save(user);
+        return JoinResponse.from(user);
     }
 }
