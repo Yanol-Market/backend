59fe79f55e875150b68a477993abbfbd6532876d
cyPark95 <parkcy1007@gmail.com>
2024-01-13 18:45:33 +0900

    fix: H2-Console 별도의 Request Matcher 등록

--- src/main/java/site/goldenticket/common/security/SecurityConfiguration.java
+++ src/main/java/site/goldenticket/common/security/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package site.goldenticket.common.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -38,13 +37,17 @@ import static org.springframework.http.HttpMethod.*;
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
-    private static final String[] PERMIT_ALL_GET_URLS = new String[] {
+    private static final String[] PERMIT_ALL_URLS = new String[]{
+            "/h2-console/**",
+    };
+
+    private static final String[] PERMIT_ALL_GET_URLS = new String[]{
             "/favicon.ico",
             "/docs/**",
             "/users/check/**"
     };
 
-    private static final String[] PERMIT_ALL_POST_URLS = new String[] {
+    private static final String[] PERMIT_ALL_POST_URLS = new String[]{
             "/users"
     };
 
@@ -67,9 +70,9 @@ public class SecurityConfiguration {
                 .headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(PERMIT_ALL_URLS).permitAll()
                         .requestMatchers(GET, PERMIT_ALL_GET_URLS).permitAll()
                         .requestMatchers(POST, PERMIT_ALL_POST_URLS).permitAll()
-                        .requestMatchers(PathRequest.toH2Console()).permitAll()
                         .anyRequest().authenticated()
                 )
                 .with(
