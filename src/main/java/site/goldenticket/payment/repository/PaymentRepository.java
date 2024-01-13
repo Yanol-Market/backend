c4704ad93ab5e551f02b97852ef58c52a3b5614a
Su-daa <subin.kk019@gmail.com>
2024-01-14 00:38:58 +0900

    rename: Payment 폴더 domain 하위로 이동

--- src/main/java/site/goldenticket/payment/repository/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package site.goldenticket.payment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import site.goldenticket.payment.model.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
