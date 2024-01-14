47204d7c7058be36b3396eeddc9ddec3a071fe33
dldmldlsy <dldmldlsy@naver.com>
2024-01-15 03:57:55 +0900

    feat: 채팅방 상세 조회 API 구현
    - 채팅방 양도 상품 상단 정보
    - 채팅 내역 (시스템 메세지는 유저에 따라 출력여부 구분)

--- src/main/java/site/goldenticket/domain/chat/controller/ChatController.java
+++ src/main/java/site/goldenticket/domain/chat/controller/ChatController.java
@@ -1,12 +1,28 @@
 package site.goldenticket.domain.chat.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import site.goldenticket.common.response.CommonResponse;
+import site.goldenticket.domain.chat.dto.ChatRoomDetailResponse;
+import site.goldenticket.domain.chat.service.ChatService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/chats")
 public class ChatController {
+
+    private final ChatService chatService;
+
+    @GetMapping("/{chatRoomId}")
+    public ResponseEntity<CommonResponse<ChatRoomDetailResponse>> getChatRoom(
+        @PathVariable Long chatRoomId) {
+        Long userId = 1L; //시큐리티 적용 후 수정 예정
+        return ResponseEntity.ok(CommonResponse.ok("채팅방 상세 정보를 조회했습니다",
+            chatService.getChatRoomDetail(userId, chatRoomId)));
+    }
 
 }
