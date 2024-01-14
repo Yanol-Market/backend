41a7768b6ff9a9c450429c9f56b6b96fdeecc86b
Su-daa <subin.kk019@gmail.com>
2024-01-14 19:32:46 +0900

    feat: Product 에 noArgsConstructor 추가

--- src/main/java/site/goldenticket/domain/product/model/Product.java
+++ src/main/java/site/goldenticket/domain/product/model/Product.java
@@ -1,8 +1,10 @@
 package site.goldenticket.domain.product.model;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import site.goldenticket.common.constants.AreaCode;
 import site.goldenticket.common.constants.ProductStatus;
 import site.goldenticket.common.constants.ReservationType;
@@ -12,6 +14,7 @@ import java.time.LocalTime;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product {
     @Id
     @Column(name = "product_id")
