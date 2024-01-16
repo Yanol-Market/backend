802789eab6fe1fb572f71b4f13a7fc18346dc084
yurim0628 <dbfla0303@naver.com>
2024-01-17 06:28:42 +0900

    feat: get 및 update 메서드 추가

--- src/main/java/site/goldenticket/common/api/RestTemplateService.java
+++ src/main/java/site/goldenticket/common/api/RestTemplateService.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface RestTemplateService {
-    
+
     <T> Optional<T> get(String url, Class<T> type);
     <T> List<T> getList(String url, Class<T[]> type);
     <T, R> Optional<T> post(String url, R request, Class<T> type);
