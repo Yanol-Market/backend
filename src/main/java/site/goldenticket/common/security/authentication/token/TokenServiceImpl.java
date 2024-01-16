cc4d0d56ca840acc1c7250e393cd89ad97d37954
cyPark95 <parkcy1007@gmail.com>
2024-01-16 21:22:08 +0900

    fix: 재발급 받은 RefreshToken 재사용 불가능 하도록 유효성 검사

--- src/main/java/site/goldenticket/common/security/authentication/token/TokenServiceImpl.java
+++ src/main/java/site/goldenticket/common/security/authentication/token/TokenServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 import site.goldenticket.common.exception.CustomException;
 import site.goldenticket.common.redis.service.RedisService;
 import site.goldenticket.common.security.authentication.dto.AuthenticationToken;
+import site.goldenticket.common.security.authentication.token.dto.RefreshToken;
 import site.goldenticket.common.security.authentication.token.dto.Token;
 import site.goldenticket.common.security.exception.InvalidJwtException;
 import site.goldenticket.common.security.exception.SaveTokenException;
@@ -30,18 +31,22 @@ public class TokenServiceImpl implements TokenService {
     private final RedisService redisService;
 
     @Override
-    public AuthenticationToken reissueToken(String refreshToken) {
+    public AuthenticationToken reissueToken(String token) {
         try {
-            String randomToken = tokenProvider.getSubject(refreshToken);
+            String randomToken = tokenProvider.getSubject(token);
             log.info("Fetch randomToken = [{}]", randomToken);
 
-            String email = redisService.get(REDIS_REFERS_TOKEN_PREFIX + randomToken, String.class)
+            RefreshToken refreshToken = redisService.get(REDIS_REFERS_TOKEN_PREFIX + randomToken, RefreshToken.class)
                     .orElseThrow(() -> new CustomException(INVALID_TOKEN));
-            log.info("Fetch Email = [{}]", email);
+            log.info("Fetch Email = [{}]", refreshToken.email());
 
-            return generatedToken(randomToken, email);
+            if (!token.equals(refreshToken.refreshToken())) {
+                throw new CustomException(INVALID_TOKEN);
+            }
+
+            return generatedToken(randomToken, refreshToken.email());
         } catch (ExpiredJwtException e) {
-            log.info("[{}] 이미 만료된 토큰입니다.", refreshToken);
+            log.info("[{}] 이미 만료된 토큰입니다.", token);
             throw new CustomException(EXPIRED_TOKEN);
         } catch (UnsupportedJwtException | MalformedJwtException | SignatureException | IllegalArgumentException e) {
             throw new CustomException(INVALID_TOKEN);
@@ -53,7 +58,8 @@ public class TokenServiceImpl implements TokenService {
         Token token = tokenProvider.generateToken(randomToken, email);
 
         try {
-            redisService.set(REDIS_REFERS_TOKEN_PREFIX + randomToken, email, token.refreshTokenExpired());
+            RefreshToken refreshToken = createRefreshToken(email, token);
+            redisService.set(REDIS_REFERS_TOKEN_PREFIX + randomToken, refreshToken, token.refreshTokenExpired());
         } catch (Exception e) {
             log.error(e.getMessage());
             throw new SaveTokenException(SAVE_REFRESH_TOKEN_FAILED);
@@ -104,5 +110,12 @@ public class TokenServiceImpl implements TokenService {
     @Override
     public boolean isBlackListToken(String token) {
         return redisService.get(REDIS_BLACK_LIST_PREFIX + token, String.class).isPresent();
+    }
+
+    private RefreshToken createRefreshToken(String email, Token token) {
+        return RefreshToken.builder()
+                .email(email)
+                .refreshToken(token.refreshToken())
+                .build();
     }
 }
