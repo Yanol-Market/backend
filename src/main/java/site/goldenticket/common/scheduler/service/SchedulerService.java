f54d95dc38dbeb3cf8be9f89bf87a2b1ee64c51d
yurim0628 <dbfla0303@naver.com>
2024-01-16 14:13:38 +0900

    refactor: 상품 SchedulerService 수정

--- src/main/java/site/goldenticket/common/scheduler/service/SchedulerService.java
+++ src/main/java/site/goldenticket/common/scheduler/service/SchedulerService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import site.goldenticket.domain.product.service.ProductSchedulerService;
 import site.goldenticket.domain.product.service.ProductService;
 
 @Slf4j
@@ -11,17 +12,17 @@ import site.goldenticket.domain.product.service.ProductService;
 @RequiredArgsConstructor
 public class SchedulerService {
 
-    private final ProductService productService;
+    private final ProductSchedulerService productSchedulerService;
 
     @Scheduled(cron = "59 59 23 * * *")
     public void updateViewCountsScheduler() {
         log.info("매일 23시 59분 59초에 실행 되는 Product ViewCount Update Scheduler.");
-        productService.updateViewCounts();
+        productSchedulerService.updateViewCounts();
     }
 
     @Scheduled(cron = "0 0 0 * * *")
     public void updateProductStatusScheduler() {
         log.info("매일 24시 00분 00초에 실행 되는 Product Status Update Scheduler.");
-        productService.updateProductStatus();
+        productSchedulerService.updateProductStatus();
     }
 }
