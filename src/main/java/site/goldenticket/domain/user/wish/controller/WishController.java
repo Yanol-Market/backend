bd983df99de120fda1e6cd0dbf44b63838deea04
cyPark95 <parkcy1007@gmail.com>
2024-01-17 23:41:01 +0900

    feat: 관심 상품 저장 API 구현

---
+++ src/main/java/site/goldenticket/domain/user/wish/controller/WishController.java
@@ -0,0 +1,31 @@
+package site.goldenticket.domain.user.wish.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import site.goldenticket.common.response.CommonResponse;
+import site.goldenticket.domain.security.PrincipalDetails;
+import site.goldenticket.domain.user.wish.dto.WishProductSaveRequest;
+import site.goldenticket.domain.user.wish.service.WishService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users/wishes")
+public class WishController {
+
+    private final WishService wishService;
+
+    @PostMapping("/product")
+    public ResponseEntity<CommonResponse<Long>> saveWishProduct(
+            @RequestBody WishProductSaveRequest wishProductSaveRequest,
+            @AuthenticationPrincipal PrincipalDetails principalDetails
+    ) {
+        Long userId = principalDetails.getUserId();
+        Long wishId = wishService.saveWishProduct(userId, wishProductSaveRequest.productId());
+        return ResponseEntity.ok(CommonResponse.ok(wishId));
+    }
+}
