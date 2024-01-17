1620f5b74ebbc857e88f81fa54ad8bcb0df38040
Su-daa <subin.kk019@gmail.com>
2024-01-17 21:23:05 +0900

    feat: userId, 주문상태로 주문 내역들 가져오기

--- src/main/java/site/goldenticket/domain/payment/repository/OrderRepository.java
+++ src/main/java/site/goldenticket/domain/payment/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package site.goldenticket.domain.payment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import site.goldenticket.common.constants.OrderStatus;
 import site.goldenticket.domain.payment.model.Order;
 
 import java.util.List;
@@ -8,4 +9,6 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findByUserId(Long userId);
+
+    List<Order> findByUserIdAndStatus(Long userId, OrderStatus status);
 }
