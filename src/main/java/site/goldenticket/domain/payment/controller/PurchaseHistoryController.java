4aa6f5f7aeb30149bbc3ad0566af86897462e738
Su-daa <subin.kk019@gmail.com>
2024-01-17 21:27:45 +0900

    feat: 구매내역-구매완료 전체조회, 상세조회

--- src/main/java/site/goldenticket/domain/payment/controller/PurchaseHistoryController.java
+++ src/main/java/site/goldenticket/domain/payment/controller/PurchaseHistoryController.java
@@ -3,10 +3,15 @@ package site.goldenticket.domain.payment.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import site.goldenticket.domain.payment.dto.response.PurchaseCompletedDetailResponse;
+import site.goldenticket.domain.payment.dto.response.PurchaseCompletedResponse;
 import site.goldenticket.domain.payment.service.PurchaseHistoryService;
 import site.goldenticket.domain.security.PrincipalDetails;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/orders/history")
@@ -19,4 +24,18 @@ public class PurchaseHistoryController {
             @AuthenticationPrincipal PrincipalDetails principalDetails) {
         return purchaseHistoryService.getPurchaseProgressHistory(principalDetails);
     }
+
+    @GetMapping("/completed")
+    public List<PurchaseCompletedResponse> getPurchaseCompletedHistory(
+            @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        return purchaseHistoryService.getPurchaseCompletedHistory(principalDetails);
+    }
+
+    @GetMapping("/completed/{orderId}")
+    public PurchaseCompletedDetailResponse getPurchaseCompletedHistoryDetail(
+            @PathVariable(name = "orderId") final Long orderId,
+            @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        return purchaseHistoryService.getPurchaseCompletedHistoryDetail(orderId,principalDetails);
+    }
+
 }
