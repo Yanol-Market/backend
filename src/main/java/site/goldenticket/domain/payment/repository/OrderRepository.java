06a487c1607d906d9be6b96c5b996b9d1657dcb5
Su-daa <subin.kk019@gmail.com>
2024-01-17 18:17:16 +0900

    feat: 유저의 주문 내역 가져오기

--- src/main/java/site/goldenticket/domain/payment/repository/OrderRepository.java
+++ src/main/java/site/goldenticket/domain/payment/repository/OrderRepository.java
@@ -3,5 +3,9 @@ package site.goldenticket.domain.payment.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.goldenticket.domain.payment.model.Order;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByUserId(Long userId);
 }
