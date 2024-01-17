41e82fa250df11e955cd56eb54ea01294da20256
cyPark95 <parkcy1007@gmail.com>
2024-01-18 01:47:19 +0900

    refactor: 중복 메서드 삭제

--- src/main/java/site/goldenticket/common/response/ErrorCode.java
+++ src/main/java/site/goldenticket/common/response/ErrorCode.java
@@ -55,7 +55,6 @@ public enum ErrorCode {
     ALREADY_EXIST_EMAIL(BAD_REQUEST, "이미 사용중인 이메일입니다. 이미 가입하신 적이 있다면 로그인을 시도해주세요"),
     ALREADY_EXIST_NICKNAME(BAD_REQUEST, "이미 사용중인 아이디입니다."),
     USER_NOT_FOUND(NOT_FOUND, "유저 정보가 존재하지 않습니다"),
-    NOT_FOUND_USER(BAD_REQUEST, "존재하지 않는 사용자입니다."),
     ALREADY_REGISTER_YANOLJA_ID(BAD_REQUEST, "이미 등록된 야놀자 아이디가 존재합니다."),
 
     // Wish
