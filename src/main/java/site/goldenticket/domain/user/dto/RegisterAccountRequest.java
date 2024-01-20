d61c902a66603d82f76fb9bcb29e8c91cf33bf63
cyPark95 <parkcy1007@gmail.com>
2024-01-20 15:02:15 +0900

    feat: 계좌 등록 요청 예외 메시지 추가

--- src/main/java/site/goldenticket/domain/user/dto/RegisterAccountRequest.java
+++ src/main/java/site/goldenticket/domain/user/dto/RegisterAccountRequest.java
@@ -3,9 +3,9 @@ package site.goldenticket.domain.user.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record RegisterAccountRequest(
-        @NotBlank
+        @NotBlank(message = "은행명을 선택해주세요.")
         String bankName,
-        @NotBlank
+        @NotBlank(message = "계좌번호를 입력해주세요.")
         String accountNumber
 ) {
 }
