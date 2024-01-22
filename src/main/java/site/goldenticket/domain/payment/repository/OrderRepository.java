71297bdd0302b6fef17fa172f98bef4cceaa691a
Su-daa <subin.kk019@gmail.com>
2024-01-22 16:22:39 +0900

    feat: 안쓰는 메서드 삭제

--- src/main/java/site/goldenticket/domain/payment/repository/OrderRepository.java
+++ src/main/java/site/goldenticket/domain/payment/repository/OrderRepository.java
@@ -1,10 +1,11 @@
 package site.goldenticket.domain.payment.repository;
 
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.goldenticket.common.constants.OrderStatus;
 import site.goldenticket.domain.payment.model.Order;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
@@ -13,8 +14,6 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByStatusAndProductId(OrderStatus status, Long productId);
 
     Order findByProductId(Long productId);
-
-    List<Order> findByUserId(Long userId);
 
     List<Order> findByUserIdAndStatus(Long userId, OrderStatus status);
 }
