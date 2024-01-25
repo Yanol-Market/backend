54a93badcaf46558654fc0386724ff570af7af0a
yurim0628 <dbfla0303@naver.com>
2024-01-25 13:03:26 +0900

    test: 테스트 코드 작성

--- src/test/java/site/goldenticket/domain/product/controller/ProductControllerTest.java
+++ src/test/java/site/goldenticket/domain/product/controller/ProductControllerTest.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,16 +20,15 @@ import site.goldenticket.domain.payment.repository.OrderRepository;
 import site.goldenticket.domain.product.model.Product;
 import site.goldenticket.domain.product.repository.ProductRepository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static site.goldenticket.common.constants.OrderStatus.WAITING_TRANSFER;
+import static site.goldenticket.common.utils.ChatRoomUtils.createChatRoom;
+import static site.goldenticket.common.utils.ChatUtils.createChat;
+import static site.goldenticket.common.utils.NegoUtils.createNego;
+import static site.goldenticket.common.utils.OrderUtils.createOrder;
 import static site.goldenticket.common.utils.ProductUtils.createProduct;
-import static site.goldenticket.domain.chat.entity.SenderType.BUYER;
-import static site.goldenticket.domain.nego.status.NegotiationStatus.NEGOTIATING;
-import static site.goldenticket.domain.nego.status.NegotiationStatus.TRANSFER_PENDING;
 import static site.goldenticket.domain.product.constants.ProductStatus.EXPIRED;
 import static site.goldenticket.domain.product.constants.ProductStatus.SOLD_OUT;
 
@@ -54,18 +54,13 @@ public class ProductControllerTest extends ApiTest {
         // given
         Product product = saveProduct();
 
+        String url = "/products/" + product.getId();
+
         // when
-        final ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .when().get("/products/{productId}", product.getId())
-                .then().log().all()
-                .extract();
+        final ExtractableResponse<Response> response = performGetRequest(url, false);
 
         // then
-        final JsonPath result = response.jsonPath();
-        assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
-                () -> assertThat(result.getLong("data.productId")).isEqualTo(product.getId())
-        );
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
     @Test
@@ -73,17 +68,13 @@ public class ProductControllerTest extends ApiTest {
         // given
         Product product = saveProduct();
 
+        String url = "/products/" + product.getId();
+
         // when
-        final ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .header("Authorization", "Bearer " + accessToken)
-                .when().delete("/products/{productId}", product.getId())
-                .then().log().all()
-                .extract();
+        final ExtractableResponse<Response> response = performAuthorizedDeleteRequest(url);
 
         // then
-        assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value())
-        );
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
     @Test
@@ -94,17 +85,15 @@ public class ProductControllerTest extends ApiTest {
         saveChat(chatRoom);
         saveNego(product);
 
+        String url = "/products/history/progress";
+
         // when
-        final ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .header("Authorization", "Bearer " + accessToken)
-                .when().get("/products/history/progress")
-                .then().log().all()
-                .extract();
+        final ExtractableResponse<Response> response = performGetRequest(url, true);
 
         // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         final JsonPath result = response.jsonPath();
         assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
                 () -> assertThat(result.getList("data.productId", Long.class)).contains(product.getId()),
                 () -> assertThat(result.getList("data.chats[0].chatRoomId", Long.class)).contains(chatRoom.getId())
         );
@@ -115,17 +104,15 @@ public class ProductControllerTest extends ApiTest {
         // given
         List<Product> products = createCompletedProductsList();
 
+        String url = "/products/history/completed";
+
         // when
-        final ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .header("Authorization", "Bearer " + accessToken)
-                .when().get("/products/history/completed")
-                .then().log().all()
-                .extract();
+        final ExtractableResponse<Response> response = performGetRequest(url, true);
 
         // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         final JsonPath result = response.jsonPath();
         assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
                 () -> assertThat(result.getLong("data[0].productId")).isEqualTo(products.get(0).getId().intValue()),
                 () -> assertThat(result.getLong("data[1].productId")).isEqualTo(products.get(1).getId().intValue())
         );
@@ -139,20 +126,17 @@ public class ProductControllerTest extends ApiTest {
         saveChat(chatRoom);
         saveOrder(product);
 
+        String url = "/products/history/completed/" + product.getId();
+        String parameterName = "productStatus";
+        String parameterValues = product.getProductStatus().toString();
+
         // when
-        final ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .header("Authorization", "Bearer " + accessToken)
-                .queryParam("productStatus", product.getProductStatus())
-                .when().get("/products/history/completed/{productId}", product.getId())
-                .then().log().all()
-                .extract();
+        final ExtractableResponse<Response> response = performAuthorizedGetRequestWithQueryParam(url, parameterName, parameterValues);
 
         // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         final JsonPath result = response.jsonPath();
-        assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
-                () -> assertThat(result.getLong("data.productId")).isEqualTo(product.getId())
-        );
+        assertThat(result.getLong("data.productId")).isEqualTo(product.getId());
     }
 
     @Test
@@ -160,41 +144,30 @@ public class ProductControllerTest extends ApiTest {
         // given
         Product product = saveSoldOutProduct();
 
+        String url = "/products/history/completed/" + product.getId();
+
         // when
-        final ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .header("Authorization", "Bearer " + accessToken)
-                .when().delete("/products/history/completed/{productId}", product.getId())
-                .then().log().all()
-                .extract();
+        final ExtractableResponse<Response> response = performAuthorizedDeleteRequest(url);
 
         // then
-        assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value())
-        );
-
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
     private Product saveProduct() {
         Product product = createProduct();
-        return productRepository.save(
-                product
-        );
+        return productRepository.save(product);
     }
 
     private Product saveSoldOutProduct() {
         Product product = createProduct();
         product.setProductStatus(SOLD_OUT);
-        return productRepository.save(
-                product
-        );
+        return productRepository.save(product);
     }
 
     private Product saveExpiredProduct() {
         Product product = createProduct();
         product.setProductStatus(EXPIRED);
-        return productRepository.save(
-                product
-        );
+        return productRepository.save(product);
     }
 
     private List<Product> createCompletedProductsList() {
@@ -202,53 +175,52 @@ public class ProductControllerTest extends ApiTest {
     }
 
     private ChatRoom saveChatRoom(Product product) {
-        return chatRoomRepository.save(
-                ChatRoom.builder()
-                        .productId(product.getId())
-                        .buyerId(user.getId())
-                        .build()
-        );
+        ChatRoom chatRoom = createChatRoom(product, user);
+        return chatRoomRepository.save(chatRoom);
     }
 
     private void saveChat(ChatRoom chatRoom) {
-        chatRepository.save(
-                Chat.builder()
-                        .chatRoomId(chatRoom.getId())
-                        .senderType(BUYER)
-                        .userId(user.getId())
-                        .content("message")
-                        .build()
-        );
+        Chat chat = createChat(chatRoom, user);
+        chatRepository.save(chat);
     }
 
     private void saveNego(Product product) {
-        LocalDateTime currentTime = LocalDateTime.now();
-
-        Nego nego = Nego.builder()
-                .price(50000)
-                .count(0)
-                .status(NEGOTIATING)
-                .consent(false)
-                .expirationTime(currentTime.plusDays(7))
-                .createdAt(currentTime)
-                .updatedAt(currentTime)
-                .build();
-
-        nego.setProduct(product);
-        nego.setUser(user);
-
+        Nego nego = createNego(product, user);
         negoRepository.save(nego);
     }
 
     private void saveOrder(Product product) {
-        Order order = Order.builder()
-                .productId(product.getId())
-                .userId(user.getId())
-                .status(WAITING_TRANSFER)
-                .negoStatus(TRANSFER_PENDING)
-                .price(50000)
-                .build();
-
+        Order order = createOrder(product, user);
         orderRepository.save(order);
+    }
+
+    private ExtractableResponse<Response> performGetRequest(String url, boolean needsAuthentication) {
+        RequestSpecification requestSpecification = RestAssured.given().log().all();
+
+        if (needsAuthentication) {
+            requestSpecification.header("Authorization", "Bearer " + accessToken);
+        }
+
+        return requestSpecification
+                .when().get(url)
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> performAuthorizedGetRequestWithQueryParam(String url, String parameterName, String parameterValues) {
+        return RestAssured.given().log().all()
+                .header("Authorization", "Bearer " + accessToken)
+                .queryParam(parameterName, parameterValues)
+                .when().get(url)
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> performAuthorizedDeleteRequest(String url) {
+        return RestAssured.given().log().all()
+                .header("Authorization", "Bearer " + accessToken)
+                .when().delete(url)
+                .then().log().all()
+                .extract();
     }
 }
