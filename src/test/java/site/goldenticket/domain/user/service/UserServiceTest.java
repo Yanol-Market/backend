b145f9a1d93d127bd3c5a84f1edd821dcfe45bb6
cyPark95 <parkcy1007@gmail.com>
2024-01-26 16:01:37 +0900

    test: 회원 삭제 로직 검증

--- src/test/java/site/goldenticket/domain/user/service/UserServiceTest.java
+++ src/test/java/site/goldenticket/domain/user/service/UserServiceTest.java
@@ -7,22 +7,26 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.util.ReflectionTestUtils;
 import site.goldenticket.common.exception.CustomException;
 import site.goldenticket.domain.user.dto.JoinRequest;
+import site.goldenticket.domain.user.dto.RemoveUserRequest;
 import site.goldenticket.domain.user.entity.User;
 import site.goldenticket.domain.user.repository.UserRepository;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
-import static site.goldenticket.common.response.ErrorCode.ALREADY_EXIST_EMAIL;
-import static site.goldenticket.common.response.ErrorCode.ALREADY_EXIST_NICKNAME;
+import static site.goldenticket.common.response.ErrorCode.*;
 import static site.goldenticket.common.utils.UserUtils.*;
 
 @DisplayName("UserService 검증")
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
+
+    public static final long USER_ID = -1L;
 
     @InjectMocks
     private UserService userService;
@@ -75,5 +79,36 @@ class UserServiceTest {
         assertThatThrownBy(() -> userService.join(joinRequest))
                 .isInstanceOf(CustomException.class)
                 .hasMessage(ALREADY_EXIST_NICKNAME.getMessage());
+    }
+
+    @Test
+    @DisplayName("회원 삭제 검증")
+    void deleteUser() {
+        // given
+        User user = createUser(PASSWORD);
+        ReflectionTestUtils.setField(user, "id", USER_ID);
+
+        when(userRepository.findById(USER_ID)).thenReturn(Optional.of(user));
+        RemoveUserRequest removeUserRequest = createRemoveUserRequest();
+
+        // when
+        userService.deleteUser(USER_ID, removeUserRequest);
+
+        // then
+        assertThat(user.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("회원 삭제 실패 - 사용자 정보가 없는 경우 예외 발생")
+    void deleteUser_failureNotFoundUser() {
+        // given
+        when(userRepository.findById(USER_ID)).thenReturn(Optional.empty());
+        RemoveUserRequest removeUserRequest = createRemoveUserRequest();
+
+        // when
+        // then
+        assertThatThrownBy(() -> userService.deleteUser(USER_ID, removeUserRequest))
+                .isInstanceOf(CustomException.class)
+                .hasMessage(USER_NOT_FOUND.getMessage());
     }
 }
