f92f5756d6d1e17538cbc2062b36519ce1bd8ed6
Su-daa <subin.kk019@gmail.com>
2024-01-26 11:11:09 +0900

    feat: 모바일용 사후 검증 추가

--- src/main/java/site/goldenticket/domain/payment/controller/PaymentController.java
+++ src/main/java/site/goldenticket/domain/payment/controller/PaymentController.java
@@ -6,9 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import site.goldenticket.common.response.CommonResponse;
+import site.goldenticket.domain.payment.dto.request.PaymentRequest;
 import site.goldenticket.domain.payment.dto.response.PaymentDetailResponse;
 import site.goldenticket.domain.payment.dto.response.PaymentReadyResponse;
-import site.goldenticket.domain.payment.dto.request.PaymentRequest;
 import site.goldenticket.domain.payment.dto.response.PaymentResponse;
 import site.goldenticket.domain.payment.service.PaymentService;
 import site.goldenticket.domain.security.PrincipalDetails;
@@ -44,4 +44,18 @@ public class PaymentController {
         PaymentResponse paymentResponse = paymentService.savePayment(request, principalDetails);
         return ResponseEntity.ok(CommonResponse.ok("Payment result", paymentResponse));
     }
+
+    @PostMapping("/mobile/result")
+    public ResponseEntity<CommonResponse<PaymentResponse>> saveMobliePayment(
+            @RequestParam(name = "imp_uid") String impUid,
+            @RequestParam(name = "merchant_uid") Long orderId,
+            @AuthenticationPrincipal PrincipalDetails principalDetails
+    ) {
+        PaymentRequest request = PaymentRequest.builder()
+                .impUid(impUid)
+                .orderId(orderId)
+                .build();
+        PaymentResponse paymentResponse = paymentService.savePayment(request, principalDetails);
+        return ResponseEntity.ok(CommonResponse.ok("Payment result", paymentResponse));
+    };
 }
