b4ed02ee9adc35afc79ad147d619809c609fd53c
cyPark95 <parkcy1007@gmail.com>
2024-02-12 14:33:02 +0800

    docs: 관심지역 조회 API 명세

--- src/test/java/site/goldenticket/domain/user/wish/controller/WishRegionControllerTest.java
+++ src/test/java/site/goldenticket/domain/user/wish/controller/WishRegionControllerTest.java
@@ -22,9 +22,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.restdocs.payload.JsonFieldType.ARRAY;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.JsonFieldType.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static site.goldenticket.common.utils.UserUtils.createWishRegion;
 import static site.goldenticket.domain.product.constants.AreaCode.*;
 
@@ -77,12 +76,11 @@ class WishRegionControllerTest extends ApiDocumentation {
         userRepository.save(user);
 
         WishRegionRegisterRequest request = new WishRegionRegisterRequest(List.of(BUSAN, DAEGU));
-
         String url = "/users/regions";
 
         // when
         ExtractableResponse<Response> result = RestAssured
-                .given().log().all()
+                .given(spec).log().all()
                 .header("Authorization", "Bearer " + accessToken)
                 .contentType(APPLICATION_JSON_VALUE)
                 .body(request)
@@ -111,10 +109,23 @@ class WishRegionControllerTest extends ApiDocumentation {
 
         String url = "/users/regions";
 
+        RestDocumentationFilter document = createDocument(
+                "user/region/find/success",
+                responseFields(
+                        fieldWithPath("status").ignored(),
+                        fieldWithPath("message").ignored(),
+                        fieldWithPath("data.wishRegions[].id").type(NUMBER)
+                                .description("관심지역 식별값"),
+                        fieldWithPath("data.wishRegions[].region").type(STRING)
+                                .description("지역코드")
+                )
+        );
+
         // when
         ExtractableResponse<Response> result = RestAssured
-                .given().log().all()
+                .given(spec).log().all()
                 .header("Authorization", "Bearer " + accessToken)
+                .filter(document)
                 .when()
                 .get(url)
                 .then().log().all()
